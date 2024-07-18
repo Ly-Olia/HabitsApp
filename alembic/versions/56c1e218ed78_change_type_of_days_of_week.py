a8978ecef79575e73fae6246bd559eddfea6d0ed
Olha Zahoruiko <lyolia@olhas-laptop.home>
2024-07-18 16:40:56 +0200

    Modify code

--- alembic/versions/56c1e218ed78_change_type_of_days_of_week.py
+++ alembic/versions/56c1e218ed78_change_type_of_days_of_week.py
@@ -5,15 +5,16 @@ Revises: 61880c6e4b58
 Create Date: 2024-07-17 15:15:47.082222
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '56c1e218ed78'
-down_revision: Union[str, None] = '61880c6e4b58'
+revision: str = "56c1e218ed78"
+down_revision: Union[str, None] = "61880c6e4b58"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -21,23 +22,23 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade():
     # Change column type to ARRAY(Integer)
     op.alter_column(
-        'habits',
-        'days_of_week',
+        "habits",
+        "days_of_week",
         existing_type=sa.Integer(),
         type_=sa.ARRAY(sa.Integer),
         existing_nullable=False,
-        postgresql_using='ARRAY[days_of_week]::INTEGER[]'
+        postgresql_using="ARRAY[days_of_week]::INTEGER[]",
     )
 
 
 def downgrade():
     # Change column type back to Integer (if needed)
     op.alter_column(
-        'habits',
-        'days_of_week',
+        "habits",
+        "days_of_week",
         existing_type=sa.ARRAY(sa.Integer),
         type_=sa.Integer,
         existing_nullable=False,
-        postgresql_using='days_of_week[1]'
+        postgresql_using="days_of_week[1]",
     )
     # ### end Alembic commands ###
